a549bfb82471241f0215aee6cde7bbfd9e46c278
Roni <ronielsonmacedo@gmail.com>
2025-12-21 20:40:24 -0300

    Implement test get booking for id

--- src/test/kotlin/TesteBasico.kt
+++ src/test/kotlin/TesteBasico.kt
@@ -1,13 +1,14 @@
 
-import io.restassured.RestAssured
-import io.restassured.RestAssured.given
+import io.restassured.RestAssured.*
+import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.`is`
 import kotlin.test.Test
 
 class TesteBasico {
     @Test
     fun testGetBooking() {
         // Configura a URL base para as requisições da API
-        RestAssured.baseURI = "https://restful-booker.herokuapp.com"
+        baseURI = "https://restful-booker.herokuapp.com"
 
         // Configura e executa a requisição GET para o endpoint "/booking/"
         given() // Define as configurações da requisição (headers, parâmetros, etc.)
@@ -17,5 +18,22 @@ class TesteBasico {
         .then()
             .statusCode(200)
             .log().all()
+    }
+
+    @Test
+    fun testGetBookingWithId() {
+        baseURI = "https://restful-booker.herokuapp.com"
+
+        `when`()
+            .get("/booking/102")
+        .then()
+            .statusCode(200)
+            .body("firstname", equalTo("Josh"))
+            .body("lastname", equalTo("Allen"))
+            .body("totalprice", equalTo(111))
+            .body("depositpaid", `is`(true))
+            .body("bookingdates.checkin", equalTo("2018-01-01"))
+            .body("bookingdates.checkout", equalTo("2019-01-01"))
+            .body("additionalneeds", equalTo("super bowls"))
     }
 }
